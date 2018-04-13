d2c28e197362d8d82c736c0054f8eaa0cf99e85b
jcannas2017 <juliencannas44@gmail.com>
2018-04-13 16:58:33 +0200

    dependance pour le room essai sur la classe Agence

--- app/src/main/java/fr/eni/jcannas2017/projet_lokacar/beans/Agence.java
+++ app/src/main/java/fr/eni/jcannas2017/projet_lokacar/beans/Agence.java
@@ -1,4 +1,7 @@
 package fr.eni.jcannas2017.projet_lokacar.beans;
 
+import android.arch.persistence.room.Entity;
+
+@Entity
 public class Agence {
 }
--- app/src/main/java/fr/eni/jcannas2017/projet_lokacar/beans/Vehicule.java
+++ app/src/main/java/fr/eni/jcannas2017/projet_lokacar/beans/Vehicule.java
@@ -1,5 +1,5 @@
 package fr.eni.jcannas2017.projet_lokacar.beans;
 
 public class Vehicule {
-    
+
 }
