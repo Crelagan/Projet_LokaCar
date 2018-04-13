7738a5515338c203ce6c4f69ae580fad270409d1
jcannas2017 <juliencannas44@gmail.com>
2018-04-13 17:05:03 +0200

    ajout id classe agence

--- app/src/main/java/fr/eni/jcannas2017/projet_lokacar/beans/Agence.java
+++ app/src/main/java/fr/eni/jcannas2017/projet_lokacar/beans/Agence.java
@@ -1,7 +1,10 @@
 package fr.eni.jcannas2017.projet_lokacar.beans;
 
 import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
 
 @Entity
 public class Agence {
+    @PrimaryKey
+    private int id;
 }
